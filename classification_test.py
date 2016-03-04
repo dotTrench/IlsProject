f839adf35df0b5bb6d212d4e224adc967d1d5083
Erik Strandgard <erik.strandgard@gmail.com>
2016-03-04 01:07:21 +0100

    might be working for left_child

--- classification_test.py
+++ classification_test.py
@@ -7,11 +7,12 @@ from classifications.decision_tree_binary import *
 from pandas import read_csv
 from pprint import pprint
 # load the iris datasets
-dataset = datasets.load_iris()
+# dataset = datasets.load_iris()
 
 # dt = DecisionTreeClassifier()
 # dt.fit(dataset.data, dataset.target)
-get_max_gini_index(np.array([[3, 3], [2, 1], [1, 9], [4, 110]]), np.array(['T', 'F', 'F', 'T']))
+t = build_tree(np.array([[3, 3], [2, 1], [1, 9], [4, 5]]), np.array(['T', 'F', 'F', 'T']))
+pprint(t.left_child)
 # v = gini_calc([3, 2, 1, 4], ['T', 'F', 'F', 'T'])
 # print(v)
 # binTree = DecisionTreeBinary()
--- classifications/decision_tree_binary.py
+++ classifications/decision_tree_binary.py
@@ -4,9 +4,71 @@ import math
 import time
 
 
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.left_child = None
+        self.right_child = None
+
+    def is_leaf(self):
+        return self.left_child is None and self.right_child is None
+
 # |4| # F
 # |2| # T
 # |4| # T
+
+
+def get_best_attribute():
+    return 0
+
+
+def build_tree(dataset, results, depth=0):
+    depth += 1
+    print(dataset)
+    print(results)
+    if depth > 10:
+        print('too deppp')
+        return Node(0)
+    if np.count_nonzero(results == results[0]) == len(results):
+        print('results')
+        # All the elements are the same return this classification
+        return Node(results[0])
+    if len(dataset) <= 0:
+        print('XDDD')
+
+        # TODO: Return majority value
+        return Node(results[0])
+
+    attributes = [i for i in range(0, len(dataset[0]))]
+    best = get_best_attribute()
+
+    column = get_column(dataset, best)
+    index = get_split_index(column, results)
+    value = column[index]
+
+    new_left_dataset = []
+    new_left_results = []
+    for i in range(len(dataset)):
+        entry = dataset[i]
+        if entry[best] < value:
+            new_entry = []
+            new_left_results.append(results[i])
+            for i in range(len(entry)):
+                if i != best:
+                    new_entry.append(entry[i])
+            if new_entry != []:
+                new_left_dataset.append(new_entry)
+
+    node = Node(value)
+    node.left_child = build_tree(np.array(new_left_dataset), np.array(new_left_results), depth)
+    #node.right_child = build_tree(np.array(n_dataset), np.array(n_results), depth)
+
+    # Remove column
+    # node.left_child = build_tree(dataset, results)
+    # node.right_child = build_tree(dataset, results)
+
+    return node
+
 
 def get_column(data, column):
     return data[:, column]
@@ -17,16 +79,11 @@ def g_calc(dataset, results):
     column = None
     for i in range(len(dataset[0])):
         column = get_column(dataset, i)
-        print(column)
         ginis.append(gini_calc(column, results))
 
     x = max(ginis)
-    print(x)
     y = ginis.index(x)
-    print(y)
-    print(column[1])
 
-    # print(ginis)
     return ginis
 
 
@@ -37,13 +94,11 @@ def gini(results):
     return 1 - val
 
 
-def get_max_gini_index(dataset, results):
+def get_split_index(column, results):
     max_gini = -float("inf")
 
     index = 0
-    for i in range(0, len(dataset[0])):
-        column = get_column(dataset, i)
-        print('col :' + column)
+    for i in range(0, len(column)):
         gini = calc_one_gini(column, results, i)
         if gini > max_gini:
             max_gini = gini
@@ -51,11 +106,11 @@ def get_max_gini_index(dataset, results):
 
     return index
 
+
 def calc_one_gini(attribute, results, i):
     a = attribute[i]
     child1 = {}
     child2 = {}
-    print('A: ' + str(a))
     for j in range(0, len(attribute)):
         b = attribute[j]
         if b <= a:
@@ -69,17 +124,11 @@ def calc_one_gini(attribute, results, i):
             else:
                 child2[results[j]] = 1
 
-    print('Child 1: {0}'.format(child1))
-    print('Child 2: {0}'.format(child2))
-    print()
-
     child1_list = list(child1.values())
     child2_list = list(child2.values())
 
     c1_gini = gini(child1_list)
     c2_gini = gini(child2_list)
-    print('C1 GINI: {0}'.format(c1_gini))
-    print('C2 GINI: {0}'.format(c2_gini))
 
     prob_1 = sum(child1_list) / (sum(child1_list) + sum(child2_list))
     prob_2 = sum(child2_list) / (sum(child1_list) + sum(child2_list))
@@ -87,14 +136,6 @@ def calc_one_gini(attribute, results, i):
     return c1_gini * prob_1 + c2_gini * prob_2
 
 
-class Node:
-    def __init__(self, value=None):
-        self.value = value
-        self.left_child = None
-        self.right_child = None
-
-    def is_leaf(self):
-        return self.left_child is None and self.right_child is None
 
 
 class DecisionTreeBinary:
